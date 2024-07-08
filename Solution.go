44c8b9056c4a98aa4d15290de60ea710dfa8f196
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-07-08 12:20:13 +0200

    Update Solution.go

--- Solution.go
+++ Solution.go
@@ -2,22 +2,22 @@
 package main
 
 import (
-	"container/list"
-	"fmt"
+    "container/list"
+    "fmt"
 )
 
 type Point struct {
-	Row       int
-	Column    int
-	Direction int
+    Row       int
+    Column    int
+    Direction int
 }
 
 func NewPoint(row, column, direction int) *Point {
-	point := Point{}
-	point.Row = row
-	point.Column = column
-	point.Direction = direction
-	return &point
+    point := Point{}
+    point.Row = row
+    point.Column = column
+    point.Direction = direction
+    return &point
 }
 
 const EMPTY_SPACE = 0
@@ -31,66 +31,66 @@ var rows = 0
 var columns = 0
 
 func numberOfCleanRooms(room [][]int) int {
-	rows = len(room)
-	columns = len(room[0])
-	return breadthFirstSearchToFindMaxCleanedPoints(&room)
+    rows = len(room)
+    columns = len(room[0])
+    return breadthFirstSearchToFindMaxCleanedPoints(room)
 }
 
-func breadthFirstSearchToFindMaxCleanedPoints(room *[][]int) int {
-	queue := list.New()
-	queue.PushBack(NewPoint(0, 0, 0))
+func breadthFirstSearchToFindMaxCleanedPoints(room [][]int) int {
+    queue := list.New()
+    queue.PushBack(NewPoint(0, 0, 0))
 
-	visited = make([][]int, rows)
-	for i := range visited {
-		visited[i] = make([]int, columns)
-	}
+    visited = make([][]int, rows)
+    for i := range visited {
+        visited[i] = make([]int, columns)
+    }
 
-	markAsVisitedWithGivenDirection(0, 0, 0)
+    markAsVisitedWithGivenDirection(0, 0, 0)
 
-	var maxCleanedPoints = 1
+    var maxCleanedPoints = 1
 
-	for queue.Len() > 0 {
+    for queue.Len() > 0 {
 
-		var current = queue.Front().Value
-		queue.Remove(queue.Front())
+        var current = queue.Front().Value.(*Point)
+        queue.Remove(queue.Front())
 
-		var nextRow = current.(*Point).Row + currentDirectionToNextCoordinates[current.(*Point).Direction][0]
-		var nextColumn = current.(*Point).Column + currentDirectionToNextCoordinates[current.(*Point).Direction][1]
+        var nextRow = current.Row + currentDirectionToNextCoordinates[current.Direction][0]
+        var nextColumn = current.Column + currentDirectionToNextCoordinates[current.Direction][1]
 
-		if !isInRoom(nextRow, nextColumn) || (*room)[nextRow][nextColumn] == OBSTACLE {
-			var newDirection = rotate90DegreesClockwise(current.(*Point).Direction)
-			if isVisitedWithGivenDirection(current.(*Point).Row, current.(*Point).Column, newDirection) {
-				break
-			}
+        if !isInRoom(nextRow, nextColumn) || room[nextRow][nextColumn] == OBSTACLE {
+            var newDirection = rotate90DegreesClockwise(current.Direction)
+            if isVisitedWithGivenDirection(current.Row, current.Column, newDirection) {
+                break
+            }
 
-			markAsVisitedWithGivenDirection(current.(*Point).Row, current.(*Point).Column, newDirection)
-			queue.PushBack(NewPoint(current.(*Point).Row, current.(*Point).Column, newDirection))
-			continue
-		}
+            markAsVisitedWithGivenDirection(current.Row, current.Column, newDirection)
+            queue.PushBack(NewPoint(current.Row, current.Column, newDirection))
+            continue
+        }
 
-		if !isVisitedWithGivenDirection(nextRow, nextColumn, current.(*Point).Direction) {
-			if visited[nextRow][nextColumn] == 0 {
-				maxCleanedPoints++
-			}
-			markAsVisitedWithGivenDirection(nextRow, nextColumn, current.(*Point).Direction)
-			queue.PushBack(NewPoint(nextRow, nextColumn, current.(*Point).Direction))
-		}
-	}
-	return maxCleanedPoints
+        if !isVisitedWithGivenDirection(nextRow, nextColumn, current.Direction) {
+            if visited[nextRow][nextColumn] == 0 {
+                maxCleanedPoints++
+            }
+            markAsVisitedWithGivenDirection(nextRow, nextColumn, current.Direction)
+            queue.PushBack(NewPoint(nextRow, nextColumn, current.Direction))
+        }
+}
+    return maxCleanedPoints
 }
 
 func isVisitedWithGivenDirection(row, column, direction int) bool {
-	return (visited[row][column] & (1 << direction)) != 0
+    return (visited[row][column] & (1 << direction)) != 0
 }
 
 func markAsVisitedWithGivenDirection(row, column, direction int) {
-	visited[row][column] = (visited[row][column] | (1 << direction))
+    visited[row][column] = (visited[row][column] | (1 << direction))
 }
 
 func rotate90DegreesClockwise(currentDirection int) int {
-	return (currentDirection + 1) % len(directions)
+    return (currentDirection + 1) % len(directions)
 }
 
 func isInRoom(row, column int) bool {
-	return row >= 0 && row < rows && column >= 0 && column < columns
+    return row >= 0 && row < rows && column >= 0 && column < columns
 }
